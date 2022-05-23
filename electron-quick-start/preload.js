a700d86eef9259ef294c880496ab70f44ee63322
wangbingyi <1041493283@qq.com>
2022-05-23 10:43:20 +0800

    update

--- electron-quick-start/preload.js
+++ electron-quick-start/preload.js
@@ -1,12 +1,12 @@
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
+// 所有 Node.js API在预加载过程中可用。
+// 它与Chrome扩展具有相同的沙箱。
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
+    const element = document.getElementById(selector);
+    if (element) element.innerText = text;
+  };
 
   for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
+    replaceText(`${type}-version`, process.versions[type]);
   }
-})
+});
---
+++ electron11-demo-01/main.js
@@ -0,0 +1,18 @@
+﻿const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+var window = null;
+
+// 完整禁用旧样式
+process.env.ELECTRON_HIDE_INTERNAL_MODULES = 'true'
+// 或者调用API
+// require('electron').hideInternalModules()
+
+app.on('ready', function() {
+  window = new BrowserWindow({width: 800, height: 600});
+  window.loadURL('https://www.w3cschool.cn');
+});
+
+
+
